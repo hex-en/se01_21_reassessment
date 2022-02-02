2e77c8a9f42f06af4d9dde6f50e82c8cfc3fec16
Aleks Pawlak <aleks.pawlak@code.berlin>
2022-02-02 16:58:50 +0100

    Excercise2 final version

--- script.js
+++ script.js
@@ -4,44 +4,34 @@ function init() {
     for (x = 0; x < 12; x++) {
         let userInput = getInput().split("").map(Number);
 
-        let hint1 = rightPos(userInput, code);
-        let hint2 = rightGuess(userInput, code);
+        let hints = analyseInput(userInput, code);
 
-        if (hint1 === 4) {
+
+        if (hints[0] === 4) {
             alert("You broke the code")
             break;
         }
-        alert("Numbers in right possition: " + hint1 + ". Guessed numbers: " + hint2);
-        console.log(code, userInput, hint1, hint2);
+        alert("Numbers in right possition: " + hints[0] + ". Guessed numbers: " + hints[1]);
+        console.log(code, userInput, hints[0], hints[1]);
     };
 
 };
-function rightPos(userInput, code) {
+function analyseInput(userInput, code) {
     let hint1 = 0;
+    let hint2 = 0;
 
     for (i = 0; i < 4; i++) {
         if (userInput[i] === code[i]) {
             hint1++;
         }
-        //else if (code.includes(userInput[i] === true)) {
-        //     hint2++;
-        // }
-    }
-    return (hint1);
-};
-function rightGuess(userInput, code) {
-    let hint2 = 0;
-
-    for (i = 0; i < 4; i++) {
-        if (userInput[i] === code[i]) {
-
-        }
         else if (code.includes(userInput[i])) {
             hint2++;
         }
     }
-    return (hint2);
-}
+    return [hint1, hint2];
+};
+
+
 function getInput() {
     let input = prompt("Guess the numbers");
     return (input);
