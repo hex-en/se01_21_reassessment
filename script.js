34a80ad8e9ab667c14351ab8f79f95cbe807951e
Aleks Pawlak <aleks.pawlak@code.berlin>
2022-02-02 14:59:10 +0100

    string to int

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const code = [4, 4, 3, 1];
+function init() {
+    let userInput = getInput().split("").map(Number);
+    console.log(code, userInput);
+};
+function getInput() {
+    let input = prompt("Guess the numbers");
+    return (input);
+};
+
+init();
