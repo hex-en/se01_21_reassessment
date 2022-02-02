08511fd1484b6fa8ea24bef473689c3fd4871e43
Aleks Pawlak <aleks.pawlak@code.berlin>
2022-02-02 15:38:12 +0100

    first working version

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const code = [4, 4, 3, 1];
 function init() {
-    let userInput = getInput().split("").map(Number);
-    console.log(code, userInput);
+    for (x = 0; x < 12; x++) {
+        let userInput = getInput().split("").map(Number);
+        let hint = 0;
+
+        for (i = 0; i < 4; i++) {
+            if (userInput[i] === code[i]) {
+                hint++;
+            }
+        }
+        if (hint === 4) {
+            alert("You broke the code")
+            break;
+        }
+        alert("Numbers in right possition: " + hint)
+        console.log(code, userInput, hint);
+    };
+
 };
 function getInput() {
     let input = prompt("Guess the numbers");
